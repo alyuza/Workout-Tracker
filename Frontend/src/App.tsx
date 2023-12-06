7fe92ad255e806753decbbef0bffc81d2adb65af
alyuza <alyuza.sat@gmail.com>
2023-12-06 21:32:46 +0700

    add routes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Login, Register } from './pages/Auth'
+import { SideNav } from './pages/Main/Cycling'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path='/' element={< Login />} />
           <Route path='/register' element={< Register />} />
+          <Route path='/dashboard' element={< SideNav />} />
         </Routes>
       </Router>
     </>
