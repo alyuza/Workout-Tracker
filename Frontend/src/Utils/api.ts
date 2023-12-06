3eb681703e15a3a987e31547fbcfb510e55a78a3
alyuza <alyuza.sat@gmail.com>
2023-12-06 21:33:06 +0700

    modify API url

--- Frontend/src/Utils/api.ts
+++ Frontend/src/Utils/api.ts
@@ -1,5 +1,5 @@
 // development
-// export const API = 'http://localhost:3000'
+export const API = 'http://localhost:3000'
 
 // production
-export const API = 'https://us-central1-revou-batch-june.cloudfunctions.net/milestone_3_alyuza'
+// export const API = 'https://us-central1-revou-batch-june.cloudfunctions.net/milestone_3_alyuza'
