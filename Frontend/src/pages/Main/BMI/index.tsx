66b9bfc0506045d526ec887ed7dfd59f351e9a0c
alyuza <alyuza.sat@gmail.com>
2023-12-21 23:43:28 +0700

    modify BMI calculator FE

--- Frontend/src/pages/Main/BMI/index.tsx
+++ Frontend/src/pages/Main/BMI/index.tsx
@@ -1,29 +1,210 @@
-import { useState } from 'react';
-import BMIForm from '../../../components/BMI';
+import React, { useEffect, useState } from 'react';
+import {
+  Container,
+  Typography,
+  Paper,
+  TextField,
+  Button,
+  Table,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  TablePagination,
+  Grid,
+} from '@mui/material';
+import axios from 'axios';
+import { API } from '../../../utils/API';
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import Footer from '../../../components/Footer';
+import SideNav from '../../../components/SideNavbar';
 
-function App() {
-  const [bmi, setBMI] = useState<number | null>(null);
+interface BmiData {
+  weight: number;
+  height: number;
+  age: number;
+  bmi: number;
+  category: string;
+  date: string;
+}
 
-  const calculateBMI = async (height: number, weight: number) => {
-    const response = await fetch('http://localhost:5000/calculateBMI', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ height, weight }),
+const BmiCalculator: React.FC = () => {
+  const navigate = useNavigate();
+  const validate = localStorage.getItem('token');
+  if (!validate) {
+    navigate('/');
+  }
+
+  useEffect(() => {
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const [formData, setFormData] = useState({ weight: 0, height: 0, age: 0 });
+  const [history, setHistory] = useState<BmiData[]>([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage] = useState(6); // Change this to your preferred number
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(API + '/api/bmicalculator', {
+        headers: { Authorization: `Bearer ${validate}` },
+      });
+
+      if (Array.isArray(response.data.data)) {
+        setHistory(response.data.data);
+      } else {
+        console.error('Invalid response data');
+      }
+    } catch (error) {
+      console.error('Error fetching BMI data:', error);
+    }
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: parseFloat(event.target.value) || 0,
     });
+  };
 
-    const data = await response.json();
-    setBMI(data.bmi);
+  const handleCalculateBmi = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(API + '/api/bmicalculator', formData, {
+        headers: { Authorization: `Bearer ${validate}` },
+      });
+
+      setHistory([...history, response.data]);
+      Swal.fire('Success!');
+      console.log('Calculating BMI...');
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
+    } catch (error) {
+      console.error('Error calculating BMI:', error);
+      Swal.fire('Failed!');
+    }
+  };
+
+  const handleClearHistory = async () => {
+    try {
+      Swal.fire({
+        title: 'Are you sure to clear history?',
+        showCancelButton: true,
+        confirmButtonText: 'Yes',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          axios.delete(API + '/api/bmicalculator', {
+            headers: { Authorization: `Bearer ${validate}` },
+          });
+          setHistory([]);
+        }
+      });
+    } catch (error) {
+      console.error('Error clearing BMI history:', error);
+      Swal.fire('Failed to clear history!');
+    }
+  };
+
+  const handleChangePage = (_event: unknown, newPage: number) => {
+    setPage(newPage);
   };
 
   return (
-    <div>
-      <h1>BMI Calculator</h1>
-      <BMIForm onCalculate={calculateBMI} />
-      {bmi !== null && <p>Your BMI is: {bmi.toFixed(2)}</p>}
-    </div>
+    <>
+      <SideNav />
+      <Container maxWidth="lg">
+        <Grid container spacing={3} minHeight={'100vh'}>
+          <Grid item xs={12} sm={6}>
+            <Paper elevation={3} style={{ padding: '20px', marginTop: '20px' }}>
+              <Typography variant="h5" gutterBottom>
+                BMI Calculator
+              </Typography>
+              <TextField
+                label="Weight (kg)"
+                type="number"
+                name="weight"
+                value={formData.weight}
+                onChange={handleInputChange}
+                fullWidth
+                margin="normal"
+              />
+              <TextField
+                label="Height (cm)"
+                type="number"
+                name="height"
+                value={formData.height}
+                onChange={handleInputChange}
+                fullWidth
+                margin="normal"
+              />
+              <TextField
+                label="Age"
+                type="number"
+                name="age"
+                value={formData.age}
+                onChange={handleInputChange}
+                fullWidth
+                margin="normal"
+              />
+              <Button variant="contained" type="submit" color="primary" onClick={handleCalculateBmi}>
+                Calculate BMI
+              </Button>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper elevation={3} style={{ padding: '20px', marginTop: '20px' }}>
+              <TableContainer>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Date</TableCell>
+                      <TableCell>Weight (Kg)</TableCell>
+                      <TableCell>Height (Cm)</TableCell>
+                      <TableCell>BMI</TableCell>
+                      <TableCell>Category</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {history
+                      .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                      .map((entry) => (
+                        <TableRow key={entry.date}>
+                          <TableCell>{entry.date}</TableCell>
+                          <TableCell>{entry.weight}</TableCell>
+                          <TableCell>{entry.height}</TableCell>
+                          <TableCell>{entry.bmi.toFixed(1)}</TableCell>
+                          <TableCell>{entry.category}</TableCell>
+                        </TableRow>
+                      ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+              <TablePagination
+                rowsPerPageOptions={[6]}
+                component="div"
+                count={history.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+              />
+              <Button
+                variant="contained"
+                color="secondary"
+                onClick={handleClearHistory}
+              >
+                Clear History
+              </Button>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+      <Footer />
+    </>
   );
-}
+};
 
-export default App;
+export default BmiCalculator;
