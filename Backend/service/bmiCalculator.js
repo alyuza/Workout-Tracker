7170bdb183ccf79ccc92d517c389dff1ab458a25
alyuza <alyuza.sat@gmail.com>
2023-12-21 17:45:41 +0700

    modify get and post

--- Backend/service/bmiCalculator.js
+++ Backend/service/bmiCalculator.js
@@ -2,16 +2,15 @@ const { ObjectId } = require("mongodb");
 
 const getBmiCalculator = async (req, res) => {
 	const usernameInput = req.username;
-	const roleInput = req.role;
 	try {
 		let workoutlist;
-		if (roleInput === "user") {
+		if (usernameInput === "admin") {
+			workoutlist = await req.db.collection("calculator").find().toArray();
+		} else {
 			workoutlist = await req.db
 				.collection("calculator")
 				.find({ maker: usernameInput })
 				.toArray();
-		} else {
-			workoutlist = await req.db.collection("calculator").find().toArray();
 		}
 		res.status(200).json({
 			message: "Success Read All BMI List",
@@ -23,51 +22,55 @@ const getBmiCalculator = async (req, res) => {
 };
 
 const bmiCalculator = async (req, res) => {
-	try {
-		const usernameInput = req.username;
-		const { weight, height, age } = req.body;
+  try {
+    const usernameInput = req.username;
+    const { weight, height, age } = req.body;
 
-		if (!weight || !height || !age) {
-			return { index, error: "Please provide weight, height, and age." };
-		}
+    if (!weight || !height || !age) {
+      return res.status(400).json({ error: "Please provide weight, height, and age." });
+    }
 
-		const weightInt = parseInt(weight, 10);
-		const heightInt = parseInt(height, 10);
-		const ageInt = parseInt(age, 10);
+    const weightInt = parseInt(weight, 10);
+    const heightInt = parseInt(height, 10);
+    const ageInt = parseInt(age, 10);
 
-		const heightInMeters = height / 100;
-		const bmi = weight / (heightInMeters * heightInMeters);
+    const heightInMeters = height / 100;
+    const bmi = weight / (heightInMeters * heightInMeters);
 
-		let category;
-		if (bmi < 18.5) {
-			category = "Underweight";
-		} else if (bmi < 24.9) {
-			category = "Normal weight";
-		} else if (bmi < 29.9) {
-			category = "Overweight";
-		} else {
-			category = "Obese";
-		}
+    let category;
+    if (bmi < 18.5) {
+      category = "Underweight";
+    } else if (bmi < 24.9) {
+      category = "Normal weight";
+    } else if (bmi < 29.9) {
+      category = "Overweight";
+    } else {
+      category = "Obese";
+    }
 
-		const newCalculate = await req.db.collection("calculator").insertOne({
-			weight: weightInt,
-			height: heightInt,
-			age: ageInt,
-			bmi,
-			category,
-			date: new Date(),
-			maker: usernameInput,
-		});
+    const currentDate = new Date();
+    const formattedDate = currentDate.toLocaleDateString().replace(/\//g, '-');
 
-		res.status(200).json({
-			ID: newCalculate.insertedId,
-			message: `BMI Calculate Result`,
-			data: newCalculate,
-		});
-	} catch (error) {
-		res.status(400).json({ error: error.message });
-	}
+    const newCalculate = await req.db.collection("calculator").insertOne({
+      weight: weightInt,
+      height: heightInt,
+      age: ageInt,
+      bmi,
+      category,
+      date: formattedDate,
+      maker: usernameInput,
+    });
+
+    res.status(200).json({
+      ID: newCalculate.insertedId,
+      message: `BMI Calculate Result`,
+      data: newCalculate,
+    });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
+
 
 const deleteBmiCalculator = async (req, res) => {
 	const id = req.params.id;
