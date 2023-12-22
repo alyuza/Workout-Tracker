d2ee8ad5f91724def8b11d7ed30abcfaea962a3b
alyuza <alyuza.sat@gmail.com>
2023-12-22 09:38:46 +0700

    modify cors

--- Backend/middleware/cors.js
+++ Backend/middleware/cors.js
@@ -1,12 +1,9 @@
 const corsOptions = {
 	origin: [
 		"http://localhost:5173",
-		"http://localhost:3000",
-		"http://localhost:3000/*",
-		"https://upset-pocket-lion.cyclic.app/",
-		"https://upset-pocket-lion.cyclic.app/*",
-		"https://workout-tracker-iy79.vercel.app/",
-		"https://workout-tracker-iy79.vercel.app/*"
+		"https://workout-tracker-server-navy.vercel.app",
+		"https://workout-tracker-server-navy.vercel.app/",
+		"https://workout-tracker-server-navy.vercel.app/*"
 	],
 	methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
 };
