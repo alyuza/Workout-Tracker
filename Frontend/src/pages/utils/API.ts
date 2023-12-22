da8af522be79a343eb955bcd5b663015648e99f8
alyuza <alyuza.sat@gmail.com>
2023-12-22 09:44:08 +0700

    try

--- Frontend/src/pages/utils/API.ts
+++ Frontend/src/pages/utils/API.ts
@@ -2,4 +2,4 @@
 // export const API = 'http://localhost:3000'
 
 // production
-export const API = 'https://workout-tracker-server-navy.vercel.app/'
+export const API = 'https://workout-tracker-server-navy.vercel.app'
