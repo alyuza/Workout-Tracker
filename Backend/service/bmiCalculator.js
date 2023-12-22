90ff1ee5a6fbb5661eea55378b82d3f2361d2628
Ireng Febrian <131000271+febriaan22@users.noreply.github.com>
2023-12-22 17:35:23 +0700

    bmi calculator backend update

--- Backend/service/bmiCalculator.js
+++ Backend/service/bmiCalculator.js
@@ -22,70 +22,73 @@ const getBmiCalculator = async (req, res) => {
 };
 
 const bmiCalculator = async (req, res) => {
-  try {
-    const usernameInput = req.username;
-    const { weight, height, age } = req.body;
+	try {
+		const usernameInput = req.username;
+		const { weight, height, age } = req.body;
 
-    if (!weight || !height || !age) {
-      return res.status(400).json({ error: "Please provide weight, height, and age." });
-    }
+		if (!weight || !height || !age) {
+			return res
+				.status(400)
+				.json({ error: "Please provide weight, height, and age." });
+		}
 
-    const weightInt = parseInt(weight, 10);
-    const heightInt = parseInt(height, 10);
-    const ageInt = parseInt(age, 10);
+		const weightInt = parseInt(weight, 10);
+		const heightInt = parseInt(height, 10);
+		const ageInt = parseInt(age, 10);
 
-    const heightInMeters = height / 100;
-    const bmi = weight / (heightInMeters * heightInMeters);
+		const heightInMeters = height / 100;
+		const bmi = weight / (heightInMeters * heightInMeters);
 
-    let category;
-    if (bmi < 18.5) {
-      category = "Underweight";
-    } else if (bmi < 24.9) {
-      category = "Normal weight";
-    } else if (bmi < 29.9) {
-      category = "Overweight";
-    } else {
-      category = "Obese";
-    }
+		let category;
+		if (bmi < 18.5) {
+			category = "Underweight";
+		} else if (bmi < 24.9) {
+			category = "Normal weight";
+		} else if (bmi < 29.9) {
+			category = "Overweight";
+		} else {
+			category = "Obese";
+		}
 
-    const currentDate = new Date();
-    const formattedDate = currentDate.toLocaleDateString().replace(/\//g, '-');
+		const currentDate = new Date();
+		const formattedDate = currentDate.toLocaleDateString().replace(/\//g, "-");
 
-    const newCalculate = await req.db.collection("calculator").insertOne({
-      weight: weightInt,
-      height: heightInt,
-      age: ageInt,
-      bmi,
-      category,
-      date: formattedDate,
-      maker: usernameInput,
-    });
+		const newCalculate = await req.db.collection("calculator").insertOne({
+			weight: weightInt,
+			height: heightInt,
+			age: ageInt,
+			bmi,
+			category,
+			date: formattedDate,
+			maker: usernameInput,
+		});
 
-    res.status(200).json({
-      ID: newCalculate.insertedId,
-      message: `BMI Calculate Result`,
-      data: newCalculate,
-    });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
+		res.status(200).json({
+			ID: newCalculate.insertedId,
+			message: `BMI Calculate Result`,
+			data: newCalculate,
+		});
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	}
 };
 
 const deleteBmiCalculator = async (req, res) => {
-  const usernameInput = req.username;
-  try {
-    const deletionConditions = {
-      $or: [{ maker: usernameInput }],
-    };
-    const deletionResult = await req.db.collection("calculator").deleteMany(deletionConditions);
-    res.status(200).json({
-      message: `${deletionResult.deletedCount} BMI documents have been deleted successfully.`,
-    });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
+	const usernameInput = req.username;
+	try {
+		const deletionConditions = {
+			$or: [{ maker: usernameInput }],
+		};
+		const deletionResult = await req.db
+			.collection("calculator")
+			.deleteMany(deletionConditions);
+		res.status(200).json({
+			message: `${deletionResult.deletedCount} BMI documents have been deleted successfully.`,
+		});
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	}
 };
-
 
 module.exports = {
 	bmiCalculator,
