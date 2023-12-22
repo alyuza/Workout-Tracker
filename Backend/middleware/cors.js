601d30468a6bf89d4e4130168ff4c2db745bc5a6
alyuza <alyuza.sat@gmail.com>
2023-12-22 09:43:27 +0700

    modify cors

--- Backend/middleware/cors.js
+++ Backend/middleware/cors.js
@@ -1,9 +1,9 @@
 const corsOptions = {
 	origin: [
-		"http://localhost:5173",
-		"https://workout-tracker-server-navy.vercel.app",
-		"https://workout-tracker-server-navy.vercel.app/",
-		"https://workout-tracker-server-navy.vercel.app/*"
+		"http://localhost:5173", // local front end
+		"https://workout-tracker-server-navy.vercel.app", //backend server
+		"https://workout-tracker-weld.vercel.app", // frontend
+		"https://https://www.versto.site" // frontend with domain
 	],
 	methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
 };
